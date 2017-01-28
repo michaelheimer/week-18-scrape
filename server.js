12ffad60d8497e2d43b5d0c9e5d5d03ebba0ba2c
Michael Heimer <michael.heimer@gmail.com>
2017-01-27 23:06:24 -0600

    almost done

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-/* Scraping into DB (18.2.5)
- * ========================== */
 
-// Initialize Express app
 var express = require('express');
 var app = express();
 var methodOverride = require('method-override');
@@ -25,6 +22,9 @@ app.set('view engine', 'handlebars');
 var mongojs = require('mongojs');
 var databaseUrl = "localhost";
 var collections = ["data"];
+
+
+
 console.log("start");
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
@@ -35,123 +35,56 @@ db.on('error', function(err) {
 
 // Main route (simple Hello World Message)
 app.get('/', function(req, res) {
-  //res.send("Hello world");
   res.render('index');
 });
 
-// Retrieve data from the db
-app.get('/all', function(req, res) {
-  // find all results from the scraoedData collection in the db
-  db.data.find({}, function(err, found) {
-    // throw any errors to the console
-    if (err) {
-      console.log(err);
-    } 
-    // if there are no errors, send the data to the browser as a json
-    else {
-      res.json(found);
-    }
-  });
-});
 
  app.get('/jokes', function(req,res){
+  
+     var jokeArray2 = [];
 
-function scrapePage () {
-    //make an HTTP request for the page to be scraped
     request(pageURL, function(error, response, html){        
-          res.send("starting scrape");
+         
+        
           var jokeObject = {
             jokeArray : []
           };
-
-         // if (!error && response.statusCode == 200) {
+        
           var $ = cheerio.load(html);
 
-           $(".jokes").each(function(i, element) {
+           $(".jokes").each(function(i, element) 
+           {
+            jokeArray2[0] = "Joke of the Day";
             var joke2 = $(element).find("p").first();
-            console.log(joke2.text() + " <<<< .jokes 2 >>>>");  
-            jokeObject.jokeArray.push(joke2.text());
+            jokeObject.jokeArray.push(joke2.text().trim());
+            jokeArray2.push(joke2.text());
            });
-
-            for(var i = 0; i < 3; i++)
-          {
-             console.log(jokeObject.jokeArray[i] + "joke # " + i);
-          }
-          res.send(jokeObject);
-
-    });
-}
-  scrapePage();
-  //res.render('jokes');
+           db.data.save({
+              jokeArray2 : jokeArray2
+           });
+          res.render('jokes',jokeObject);
+    }); 
 });
 
- app.get('/cats', function(req,res){
+ app.get('/cats', function(req,res){  // TODO
 
   console.log('inside cats');
   function strartScrape2()
   {
     request(pageURL2, function(error, response, html){
-     var $ = cheerio.load(html);
-     var $catPic = ('.thumbs');
+
+      var $ = cheerio.load(html);
+      var $catPic = ('.thumbs');
       var link = $($catPic).children().attr("href");
       console.log(link + "links");
       res.send("hello");
 
-
-
     });
-
   }
-strartScrape2();
+      strartScrape2();
 
  })
-// Scrape data from one site and place it into the mongodb db
-app.get('/scrape', function(req, res) {
-  console.log("inside scrape");
-  // make a request for the news section of ycombinator
-  request('http://www.cnn.com/', function(error, response, html) {
-    // load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // for each element with a "title" class
-    $('.title').each(function(i, element) {
-      // save the text of each link enclosed in the current element
-      var title = $(this).children('a').text();
-      console.log(title.text() + ' title');
-      res.send(title.text());
-      // save the href value of each link enclosed in the current element
-      var link = $(this).children('a').attr('href');
-      console.log(link);
 
-      // if this title element had both a title and a link
-      if (title && link) {
-        // save the data in the scrapedData db
-        db.data.save({
-          title: title,
-          link: link
-        }, 
-        function(err, saved) {
-          // if there's an error during this query
-          if (err) {
-            // log the error
-
-            console.log(err + "error");
-          } 
-          // otherwise, 
-          else {
-            // log the saved data
-            console.log(saved);
-          }
-        });
-      }
-    });
-  });
-
-  // this will send a "search complete" message to the browser
-  res.send("Scrape Complete bitches2");
-});
-
-
-// listen on port 3000
 app.listen(3000, function() {
   console.log('App running on port 3000!');
 });
